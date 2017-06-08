c6426c1379959550d3e660b248c8134c7b977c32
@jacksonsevendelta <jacksonsevendelta@gmail.com>
2017-06-08 18:15:14 +1000

    Create update.sh

--- on-startup/update.sh
+++ on-startup/update.sh
@@ -2,11 +2,13 @@
 # Clones GitHub file at https://github.com/jacksonsevendelta/on-startup to update the googstart file.
 
 #!/bin/bash
+cd
 echo 
 echo --- UPDATING PROGRAM ---
 git clone https://github.com/jacksonsevendelta/GooglePi
 cd GooglePi/on-startup
+git pull
 sudo chmod +x googstart
 echo 
-echo --- program updated (unless you don't have an Internet connection) ---
+echo --- program updated ---
 ./googstart
